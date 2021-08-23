65a22678a3aaffca249f0efb963401fd881a333f
doublenns <nnamdianyanwu@gmail.com>
2021-08-22 19:37:31 -0500

    Time: 32 ms (44.07%), Space: 5.5 MB (35.35%) - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.go
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.go
@@ -1,14 +1,10 @@
 func firstUniqChar(s string) int {
-    // Solution using a closure func
-	slice_to_map := func(s string) map[rune]int {
-		frequency := make(map[rune]int)
-		for _, v := range s {
-            frequency[v]++
-		}
-		return frequency
-	}
-
-	m := slice_to_map(s)
+    // Solution w/ all code in single func
+    m := make(map[rune]int)
+    for _, v := range s {
+        m[v]++
+    }
+    
 	for i, v := range s {
 		if m[v] == 1 {
 			return i
