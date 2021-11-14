75fb1c456a51e8b07c4b9bd5560b397698abd7b8
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 01:30:39 -0600

    Time: 68 ms (7.67%), Space: 4.4 MB (46.70%) - LeetHub

--- remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.go
@@ -1,8 +1,9 @@
 func removeDuplicates(nums []int) int {
     for i := 0; i < len(nums) -1; i++ {
         if nums[i] == nums[i+1] {
-            copy(nums[i:], nums[i+1:])
-            nums = nums[:len(nums)-1]
+            // copy(nums[i:], nums[i+1:])
+            // nums = nums[:len(nums)-1]
+            nums = append(nums[:i], nums[i+1:]...)
             i--
         }
     }
