27937793596d3c836c7c69e73c3e3d36c97cd0ce
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 01:23:19 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (81.01%) - LeetHub

---
+++ valid-parentheses/valid-parentheses.go
@@ -0,0 +1,30 @@
+func isValid(s string) bool {
+    braces := map[rune]rune{
+        '(': ')',
+        '{': '}',
+        '[': ']',
+    }
+    
+    var stack []rune
+    for _,c := range s{
+        if _, ok := braces[c]; ok {
+            stack = append(stack, c)
+        } else {
+            if len(stack) <= 0 {
+                return false
+            } else {
+                l := len(stack) -1
+                t := stack[l]
+                if braces[t] != c {
+                    return false
+                }
+                stack = stack[:l]
+            }
+        }
+    }
+    
+    if len(stack) != 0 {
+        return false
+    }
+    return true
+}
