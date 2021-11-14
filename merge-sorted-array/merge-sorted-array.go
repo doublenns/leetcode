713af821a6975daa293362ba7e5b7390e2f79f6b
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 02:55:39 -0600

    Time: 0 ms (100.00%), Space: 2.5 MB (35.66%) - LeetHub

--- merge-sorted-array/merge-sorted-array.go
+++ merge-sorted-array/merge-sorted-array.go
@@ -1,24 +1,17 @@
-import "fmt"
-
 func merge(nums1 []int, m int, nums2 []int, n int)  {
     res := []int{}
     for p1, p2 := 0,0 ; p1 < len(nums1) || p2 < len(nums2); p1, p2 = p1+1, p2+1 {
         if p1 >= len(nums1) {
             res = append(res, nums2[p2])
-            // p2++
             p1--
         } else if p2 >= len(nums2) {
             res = append(res, nums1[p1])
-            // p1++
             p2--
         } else if nums1[p1] <= nums2[p2] && p1 < m {
             res = append(res, nums1[p1])
-            // p1++
             p2--
-        // } else if nums2[p2] <= nums1[p1] {
         } else {
             res = append(res, nums2[p2])
-            // p2++
             p1--
         }
     }
