ac16379ef3670e54bb1610e09343d95b5594fc78
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 01:59:36 -0600

    Time: 4 ms (96.51%), Space: 4.3 MB (35.24%) - LeetHub

--- two-sum/two-sum.go
+++ two-sum/two-sum.go
@@ -1,14 +1,15 @@
 func twoSum(nums []int, target int) []int {
-	var result []int
-	counter := 0
+    seen := map[int]int{}
+    var result []int
+    
+
 	for i, v := range nums {
-		counter++
-		for i2, v2 := range nums[(i + 1):] {
-			if v+v2 == target {
-				result = append(result, i, i2+counter)
-				return result
-			}
-		}
+        if t, ok := seen[target - v]; ok {
+            result = append(result, t, i)
+            break
+        } else {
+            seen[v] = i
+        }
 	}
 	return result
 }
