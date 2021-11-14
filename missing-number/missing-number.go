f1e85acc19a37dce9f0dccfe136f31f9d178a751
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 01:50:29 -0600

    Time: 16 ms (89.23%), Space: 6.3 MB (99.80%) - LeetHub

--- missing-number/missing-number.go
+++ missing-number/missing-number.go
@@ -1,10 +1,10 @@
 func missingNumber(nums []int) int {
-    sort.Ints(nums)
-    
-    for i := 0; i < len(nums); i++ {
-        if nums[i] != i {
-            return i
-        }
+    // Initialize sum w/ length of input slice to account for the
+    // last element in a slice w/ no missing numbers
+    sum, numsSum := len(nums), 0
+    for i := 0; i <len(nums); i++ {
+        sum += i
+        numsSum += nums[i]
     }
-    return len(nums)
+    return sum - numsSum
 }
