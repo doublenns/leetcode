259c73375c9f9cb9e2c54ed913c955eba85046c0
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 15:51:47 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (62.30%) - LeetHub

--- to-lower-case/to-lower-case.go
+++ to-lower-case/to-lower-case.go
@@ -1,7 +1,16 @@
-// implementation using standard library
-
 import "strings"
 
 func toLowerCase(s string) string {
-    return strings.ToLower(s)
+    // 'a' == 97; 'A' == 65
+    diff := 'a' -'A'
+    var result strings.Builder
+    
+    for _, r := range s {
+        if ('A' <= r && r <= 'Z') {
+			result.WriteRune(r + diff)
+        } else {
+            result.WriteRune(r)
+        }
+	}
+	return result.String()
 }
