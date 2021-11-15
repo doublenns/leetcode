192da615c34cdb9afdedbbd484fddcd3094aca25
doublenns <nnamdianyanwu@gmail.com>
2021-11-14 23:36:02 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (56.67%) - LeetHub

--- check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.go
+++ check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.go
@@ -1,11 +1,11 @@
 func checkIfPangram(sentence string) bool {
     // Create a set of all runes in sentence and return whether 26 different
     // characters used. (Constraint indicates no punctuation or whitespace)
-    check := make(map[rune]struct{})
+    seen := make(map[rune]struct{})
     var exists = struct{}{}
     
     for _, r := range sentence {
-        check[r] = exists
+        seen[r] = exists
     }
-    return len(check) == 26
+    return len(seen) == 26
 }
