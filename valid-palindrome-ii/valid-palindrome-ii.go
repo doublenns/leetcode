3841f463aea37ff44e35921524e9b42837079dac
doublenns <nnamdianyanwu@gmail.com>
2021-11-16 23:39:51 -0600

    Time: 12 ms (91.80%), Space: 6.7 MB (99.45%) - LeetHub

---
+++ valid-palindrome-ii/valid-palindrome-ii.go
@@ -0,0 +1,21 @@
+func validPalindrome(s string) bool {
+    var validSubPalindrome func(int, int, bool) bool
+    validSubPalindrome = func(start, end int, skipAvailable bool) bool {
+        for i, j := start, end; i <= j; i, j = i+1, j-1 {
+            if s[i] != s[j] {
+                if skipAvailable {
+                    if validSubPalindrome(i+1, j, false) || validSubPalindrome(i, j-1, false) {
+                        return true
+                    } else {
+                        return false
+                    }
+                } else {
+                    return false
+                }   
+            }
+        }
+        return true
+    }
+    
+    return validSubPalindrome(0, len(s)-1, true)
+}
