ffc8a5aaf7e3c184972ef1919aa62d45c91ea2a9
doublenns <nnamdianyanwu@gmail.com>
2021-11-17 16:14:18 -0600

    Time: 0 ms (100.00%), Space: 2.1 MB (100.00%) - LeetHub

--- reverse-prefix-of-word/reverse-prefix-of-word.go
+++ reverse-prefix-of-word/reverse-prefix-of-word.go
@@ -1,16 +1,14 @@
 func reversePrefix(word string, ch byte) string {
-    var end int
-    for i := 0; i < len(word); i++ {
-        if word[i] == ch {
-            end = i
-            break
-        }
-    }
-    
+    // Get first index of ch in word
+    end := strings.IndexByte(word, ch)
+    // Strings are immutable, so convert word to slice of bytes
     bs := []byte(word)
     
+    // Reverse the chars in word up to index of ch
     for i, j := 0, end; i < j; i, j = i+1, j-1 {
         bs[i], bs[j] = bs[j], bs[i]
     }
+    
+    // Convert slice of bytes back into a string and return
     return string(bs)
 }
