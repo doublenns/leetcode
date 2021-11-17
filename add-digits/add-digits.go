29480e07a88345bf378b0ac6e9acc68e21dad2ce
doublenns <nnamdianyanwu@gmail.com>
2021-11-17 15:11:05 -0600

    Time: 0 ms (100.00%), Space: 2.2 MB (100.00%) - LeetHub

--- add-digits/add-digits.go
+++ add-digits/add-digits.go
@@ -1,28 +1,5 @@
-// Recursive Solution
-
-// func sumSlice(sl []string) int {
-//     result := 0
-//     for _, v := range sl {
-//         num, _ := strconv.Atoi(v)
-//         result += num
-//     }
-//     return result
-// }
-
-// func addDigits(num int) int {
-//     str := strconv.Itoa(num)
-//     if len(str) != 1 {
-//         nums := strings.Split(str, "")
-//         newNum := sumSlice(nums)
-//         return addDigits(newNum)
-//     }
-//     result, _ := strconv.Atoi(str)
-//     return result
-// }
-
-
 // https://math.stackexchange.com/questions/2102877/explanation-of-digital-root-sum-formula
 // O(1) time/space solution
 func addDigits(num int) int {
-    return 1 + ( (num - 1) % 9 )   
+    return 1 + ( (num - 1) % 9 )
 }
