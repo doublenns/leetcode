70dde86f43b6d2733396180136d1fcf8443e3181
doublenns <nnamdianyanwu@gmail.com>
2021-11-16 23:48:32 -0600

    Time: 8 ms (98.91%), Space: 6.7 MB (99.45%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.go
+++ valid-palindrome-ii/valid-palindrome-ii.go
@@ -1,17 +1,16 @@
 func validPalindrome(s string) bool {
-    var validSubPalindrome func(int, int, bool) bool
-    validSubPalindrome = func(start, end int, skipAvailable bool) bool {
+    var isPalindrome func(int, int, bool) bool
+    isPalindrome = func(start, end int, skipAvailable bool) bool {
         for i, j := start, end; i <= j; i, j = i+1, j-1 {
             if s[i] != s[j] {
                 if skipAvailable {
-                    return validSubPalindrome(i+1, j, false) || validSubPalindrome(i, j-1, false)
-                } else {
-                    return false
-                }   
+                    return isPalindrome(i+1, j, false) || isPalindrome(i, j-1, false)
+                }
+                return false
             }
         }
         return true
     }
     
-    return validSubPalindrome(0, len(s)-1, true)
+    return isPalindrome(0, len(s)-1, true)
 }
