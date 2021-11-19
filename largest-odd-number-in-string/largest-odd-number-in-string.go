3eae4c02df7c013b9dbb03e52019ec45acf553aa
doublenns <nnamdianyanwu@gmail.com>
2021-11-19 01:08:44 -0600

    Time: 8 ms (71.79%), Space: 6.6 MB (58.97%) - LeetHub

--- largest-odd-number-in-string/largest-odd-number-in-string.go
+++ largest-odd-number-in-string/largest-odd-number-in-string.go
@@ -1,10 +1,8 @@
 func largestOddNumber(num string) string {
-    nums := strings.Split(num, "")
-
-    for i := len(nums) -1 ; i >= 0; i-- {
-        n, _ := strconv.Atoi(nums[i])
+    for i := len(num) -1 ; i >= 0; i-- {
+        n, _ := strconv.Atoi(string(num[i]))
         if n % 2 != 0 {
-            return strings.Join(nums[:i+1], "")
+            return string(num[:i+1])
         }
     }
     
