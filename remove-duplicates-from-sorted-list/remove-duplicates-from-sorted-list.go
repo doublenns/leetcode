0c636ded61c79d28c62bb400fe19ee1ee1aa45f8
doublenns <nnamdianyanwu@gmail.com>
2021-11-20 16:17:13 -0600

    Time: 4 ms (81.00%), Space: 3.2 MB (100.00%) - LeetHub

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -1,12 +1,5 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func deleteDuplicates(head *ListNode) *ListNode {
-    if head == nil {
+    if head == nil || head.Next == nil {
         return head
     }
     
