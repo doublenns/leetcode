13c8dcb2fd820a7d3b67df8ee439ff235b44408f
doublenns <nnamdianyanwu@gmail.com>
2021-11-20 16:25:22 -0600

    Time: 4 ms (81.00%), Space: 3.2 MB (100.00%) - LeetHub

--- remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
+++ remove-duplicates-from-sorted-list/remove-duplicates-from-sorted-list.go
@@ -1,10 +1,6 @@
 func deleteDuplicates(head *ListNode) *ListNode {
-    if head == nil || head.Next == nil {
-        return head
-    }
-    
     curr := head
-    for curr.Next != nil {
+    for curr != nil && curr.Next != nil {
         temp := curr.Next
         if temp.Val == curr.Val {
             curr.Next = temp.Next
