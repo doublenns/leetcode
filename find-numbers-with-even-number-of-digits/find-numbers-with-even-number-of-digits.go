0b1605c83a600a3e401b06084697286cfa53489f
doublenns <nnamdianyanwu@gmail.com>
2021-11-21 01:11:23 -0600

    Time: 4 ms (88.03%), Space: 3.1 MB (100.00%) - LeetHub

--- find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.go
+++ find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.go
@@ -1,9 +1,18 @@
+func numberOfDigits(n int) int {
+    count := 1
+    for n >= 10 {
+        n /= 10
+        count++
+    }
+    return count
+}
+
 func findNumbers(nums []int) int {
-	count := 0
-	for _, n := range nums {
-		if len(fmt.Sprintf("%d", n))%2 == 0 {
-			count++
-		}
-	}
-	return count
+    var count int
+    for _, v := range nums {
+        if numberOfDigits(v) % 2 == 0{
+            count++
+        }
+    }
+    return count
 }
