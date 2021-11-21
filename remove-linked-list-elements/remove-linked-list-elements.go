a0c7f0ace6e6b8121767f41e5194d73a5c753966
doublenns <nnamdianyanwu@gmail.com>
2021-11-20 18:33:34 -0600

    Time: 8 ms (86.34%), Space: 4.7 MB (100.00%) - LeetHub

--- remove-linked-list-elements/remove-linked-list-elements.go
+++ remove-linked-list-elements/remove-linked-list-elements.go
@@ -1,18 +1,13 @@
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
 func removeElements(head *ListNode, val int) *ListNode {
     for head != nil && head.Val == val {
         head = head.Next
     }
-    for curr := head; curr != nil; curr = curr.Next {
+    for curr := head; curr != nil; {
         for curr.Next != nil && curr.Next.Val == val {
             curr.Next = curr.Next.Next
+            continue
         }
+        curr = curr.Next
     }
     return head
 }
