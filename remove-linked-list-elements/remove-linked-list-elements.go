3e40225ef44b82a14019f6b6ebdd9ad3be4b2640
doublenns <nnamdianyanwu@gmail.com>
2021-11-20 18:35:21 -0600

    Time: 8 ms (86.34%), Space: 4.7 MB (100.00%) - LeetHub

--- remove-linked-list-elements/remove-linked-list-elements.go
+++ remove-linked-list-elements/remove-linked-list-elements.go
@@ -2,12 +2,10 @@ func removeElements(head *ListNode, val int) *ListNode {
     for head != nil && head.Val == val {
         head = head.Next
     }
-    for curr := head; curr != nil; {
+    for curr := head; curr != nil; curr = curr.Next {
         for curr.Next != nil && curr.Next.Val == val {
             curr.Next = curr.Next.Next
-            continue
         }
-        curr = curr.Next
     }
     return head
 }
