188a37d7e2a6b971e9a0c46f519ae0c3d098d146
doublenns <nnamdianyanwu@gmail.com>
2021-11-21 01:34:52 -0600

    Time: 36 ms (46.25%), Space: 6.9 MB (65.22%) - LeetHub

--- squares-of-a-sorted-array/squares-of-a-sorted-array.go
+++ squares-of-a-sorted-array/squares-of-a-sorted-array.go
@@ -1,38 +1,7 @@
-func mergeSortedArrays(nums1 []int, m int, nums2 []int, n int) []int  {
-    res := []int{}
-    for p1, p2 := 0,0 ; p1 < len(nums1) || p2 < len(nums2); p1, p2 = p1+1, p2+1 {
-        if p1 >= len(nums1) {
-            res = append(res, nums2[p2])
-            p1--
-        } else if p2 >= len(nums2) {
-            res = append(res, nums1[p1])
-            p2--
-        } else if nums1[p1] <= nums2[p2] && p1 < m {
-            res = append(res, nums1[p1])
-            p2--
-        } else {
-            res = append(res, nums2[p2])
-            p1--
-        }
-    }
-    return res
-}
-
-func prependInt(nums []int, y int) []int {
-    nums = append(nums, 0)
-    copy(nums[1:], nums)
-    nums[0] = y
-    return nums
-}
-
 func sortedSquares(nums []int) []int {
-    var negative, positive []int
-    for _, v := range nums {
-        if v < 0 {
-            negative = prependInt(negative, v*v)
-        } else {
-            positive = append(positive, v*v)
-        }
+    for i, v := range nums {
+        nums[i] *= v
     }
-    return mergeSortedArrays(negative, len(negative), positive, len(positive))
+    sort.Ints(nums)
+    return nums
 }
