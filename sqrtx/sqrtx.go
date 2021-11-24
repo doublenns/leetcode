e77f9391f7fc6550cc02908677611ef181b9df00
doublenns <nnamdianyanwu@gmail.com>
2021-11-23 21:22:57 -0600

    Time: 0 ms (100.00%), Space: 2.2 MB (40.04%) - LeetHub

--- sqrtx/sqrtx.go
+++ sqrtx/sqrtx.go
@@ -1,13 +1,5 @@
 const DELTA = 0.01
 
-// Function abs returns the absolute value of a number passed into it
-func abs(num float64) float64 {
-	if num < 0 {
-		num = 0 - num
-	}
-	return num
-}
-
 func mySqrt(x int) int {
     if x <= 1 {
         return x
@@ -15,7 +7,7 @@ func mySqrt(x int) int {
     
     var a float64 = float64(x) / 2
     var temp float64
-    for abs(a - temp) > DELTA {
+    for math.Abs(a - temp) > DELTA {
         temp = a
         a = a - ((a*a) - float64(x)) / (2*a)
     }
