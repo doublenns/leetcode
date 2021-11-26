b852cf42b9a723d1e9630bf19b25cfc687b323a0
doublenns <nnamdianyanwu@gmail.com>
2021-11-26 03:38:44 -0500

    Time: 0 ms (100.00%), Space: 2.4 MB (100.00%) - LeetHub

--- print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
+++ print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
@@ -12,28 +12,6 @@
  *		// print the value of this node.
  *   }
  */
-type Stack struct {
-	data []ImmutableListNode
-}
-
-func (st *Stack) Push(node ImmutableListNode) bool {
-	st.data = append(st.data, node)
-	return true
-}
-
-func (st *Stack) Pop() (ImmutableListNode, error) {
-	length := len(st.data)
-	if length == 0 {
-		return nil, errors.New("empty stack")
-	}
-	rv := st.data[length-1]
-	st.data = st.data[:length-1]
-	return rv, nil
-}
-
-
-
-
 func printLinkedListInReverse(head ImmutableListNode) {
     var st []ImmutableListNode
     
@@ -43,5 +21,4 @@ func printLinkedListInReverse(head ImmutableListNode) {
     for i := len(st) -1 ; i >= 0 ; i-- {
         st[i].printValue()
     }
-    
 }
