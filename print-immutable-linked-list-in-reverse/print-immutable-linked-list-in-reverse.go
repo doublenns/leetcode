0eb19776517ce45bca07f32a169041199bc6b545
doublenns <nnamdianyanwu@gmail.com>
2021-11-26 03:44:14 -0500

    Time: 0 ms (100.00%), Space: 2.4 MB (100.00%) - LeetHub

--- print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
+++ print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
@@ -1,6 +1,47 @@
-func printLinkedListInReverse(head ImmutableListNode) {
-    for curr := head; curr != nil; curr = curr.getNext() {
-        defer curr.printValue()
-    }
+/*   Below is the interface for ImmutableListNode, which is already defined for you.
+ *
+ *   type ImmutableListNode struct {
+ *       
+ *   }
+ *
+ *   func (this *ImmutableListNode) getNext() ImmutableListNode {
+ *		// return the next node.
+ *   }
+ *
+ *   func (this *ImmutableListNode) printValue() {
+ *		// print the value of this node.
+ *   }
+ */
+type Stack struct {
+	data []ImmutableListNode
+}
 
+func (st *Stack) Push(node ImmutableListNode) bool {
+	st.data = append(st.data, node)
+	return true
+}
+
+func (st *Stack) Pop() (ImmutableListNode, error) {
+	length := len(st.data)
+	if length == 0 {
+		return nil, errors.New("empty stack")
+	}
+	rv := st.data[length-1]
+	st.data = st.data[:length-1]
+	return rv, nil
+}
+
+
+
+
+func printLinkedListInReverse(head ImmutableListNode) {
+    var st Stack
+    
+    for curr := head; curr != nil; curr = curr.getNext() {
+        st.Push(curr)
+    }
+    for v, err := st.Pop() ; err == nil; v, err = st.Pop() {
+        v.printValue()
+    }
+    
 }
