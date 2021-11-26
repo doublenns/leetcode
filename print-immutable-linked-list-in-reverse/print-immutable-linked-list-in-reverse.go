8c34702ebf5930673c715c7598018e0f78138c80
doublenns <nnamdianyanwu@gmail.com>
2021-11-26 03:43:30 -0500

    Time: 0 ms (100.00%), Space: 2.6 MB (21.43%) - LeetHub

--- print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
+++ print-immutable-linked-list-in-reverse/print-immutable-linked-list-in-reverse.go
@@ -1,17 +1,3 @@
-/*   Below is the interface for ImmutableListNode, which is already defined for you.
- *
- *   type ImmutableListNode struct {
- *       
- *   }
- *
- *   func (this *ImmutableListNode) getNext() ImmutableListNode {
- *		// return the next node.
- *   }
- *
- *   func (this *ImmutableListNode) printValue() {
- *		// print the value of this node.
- *   }
- */
 func printLinkedListInReverse(head ImmutableListNode) {
     for curr := head; curr != nil; curr = curr.getNext() {
         defer curr.printValue()
