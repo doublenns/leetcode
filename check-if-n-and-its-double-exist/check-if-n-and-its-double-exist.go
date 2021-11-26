d3281e476a6437d1bf42c2036a805832366d5dfa
doublenns <nnamdianyanwu@gmail.com>
2021-11-26 02:35:35 -0500

    Time: 10 ms (13.90%), Space: 3.4 MB (47.59%) - LeetHub

--- check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.go
+++ check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.go
@@ -5,11 +5,12 @@ func checkIfExist(arr []int) bool {
     for _, v := range arr {
         if _, ok := seen[v*2]; ok {
             return true
-        } else if _, ok := seen[v/2]; ok && v % 2 ==0 {
-            return true
-        } else {
-            seen[v] = exists
+        } else if v % 2 == 0 {
+            if _, ok := seen[v/2]; ok {
+                return true
+            }
         }
+        seen[v] = exists
     }
     return false
 }
